5f36ca3a65b220895559fcbdc17957a22290f17f
Thor Kristian Valderhaug <thor.k.valderhaug@bekk.no>
2016-01-28 12:37:51 +0100

    Add function for importing all xmls in a folder into a envirement

--- ExampleClients/dotnet/UDIR.PAS2.Example.Client/UDIR.PAS2.Example.Client/Program.cs
+++ ExampleClients/dotnet/UDIR.PAS2.Example.Client/UDIR.PAS2.Example.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Net;
@@ -13,125 +14,201 @@ using UDIR.PAS2.Example.Client.Extensions;
 
 namespace UDIR.PAS2.Example.Client
 {
-    class Program
-    {
-        [STAThread]
-        static void Main(string[] args)
-        {
-             var baseAddress = ConfigurationManager.AppSettings["environmenturl"];
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var baseAddress = ConfigurationManager.AppSettings["environmenturl"];
 
-            //obtain cookie by logging in
-            var cookie = Login(baseAddress);
-            Clipboard.SetText(cookie.ToString());
+			if (args.Length == 1)
+			{
+				var path = args[0];
+				var fileEntries = GetAllXmlFilesRecursive(path);
+				Console.WriteLine("Kommer til å importere kandidater fra {0} filer til {1}, Trykk 'y' for å gjennomføre dette?",
+					fileEntries.Count, baseAddress);
 
-            Console.WriteLine("Cookie er kopiert til utklipstavlen. Trykk en tast for å lukke dette vinduet");
+				if (!ShouldContinue())
+				{
+					Console.Error.WriteLine("Quitting...");
+					Environment.Exit(1);
+				}
 
-            Console.ReadLine();
-        }
 
-        private static void IssueRequestWithCookie(Cookie cookie, string baseAddress, string relativeAddress)
-        {
-            Console.WriteLine("Press enter to try to issue request with the newly obtained cookie");
-            Console.ReadLine();
+				foreach (var file in fileEntries)
+				{
+					ImportPåmelding(baseAddress, file);
+				}
+			}
+			else
+			{
+				var cookie = Login(baseAddress);
+				Clipboard.SetText(cookie.ToString());
+				Console.WriteLine("Cookie er kopiert til utklipstavlen. Trykk en tast for å lukke dette vinduet");
+				Console.ReadLine();
+			}
+		}
 
-            var cookieContainer = new CookieContainer();
+		private static bool ShouldContinue()
+		{
+			var info = Console.ReadKey();
+			Console.WriteLine();
+			return info.Key == ConsoleKey.Y;
+		}
 
-            if(cookie != null)
-                cookieContainer.Add(cookie);
 
-            var handler = new WebRequestHandler
-            {
-                CookieContainer = cookieContainer,
-                UseCookies = true,
-                UseDefaultCredentials = true
-            };
+		private static List<string> GetAllXmlFilesRecursive(string path)
+		{
+			var files = new List<string>();
 
-            using (var client = new HttpClient(handler))
-            {
-                client.BaseAddress = new Uri(baseAddress);
-                var response = client.GetAsync(relativeAddress).Result;
+			if (File.Exists(path))
+			{
+				files.Add(path);
+				return files;
+			}
 
-                Console.WriteLine(response);
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-            }
-        }
+			files.AddRange(Directory.GetFiles(path, "*.xml"));
 
-        private static Cookie Login(string baseAddress)
-        {
-            var xmlSignature = new XmlDocument{PreserveWhitespace = true};
+			foreach (var directory in Directory.GetDirectories(path))
+			{
+				files.AddRange(GetAllXmlFilesRecursive(directory));
+			}
+			return files;
+		}
 
-            using (var rng = new RNGCryptoServiceProvider())
-            {
-                var nonceBytes = new byte[8];
-                rng.GetBytes(nonceBytes);
+		private static void ImportPåmelding(string baseAddress, string xmlFile)
+		{
+			var xmlDoc = new XmlDocument();
+			xmlDoc.Load(xmlFile);
 
-                var nonce = Convert.ToBase64String(nonceBytes);
-                var timeStamp = DateTime.Now;
-                var clientIdentification = new ClientIdentification
-                {
-                    Skoleorgno = "875561162",
-                    Skolenavn = "Eksempel skole",
-                    Brukernavn = "skoleadmin",
-                    Nonce = nonce,
-                    TimeStamp = timeStamp
-                };
-                
-                string theSerializedString;
-                using (var ms = new MemoryStream())
-                {
-                    var xmlWriterSettings = new XmlWriterSettings()
-                    {
-                        CloseOutput = false,
-                        Encoding = Encoding.UTF8,
-                        OmitXmlDeclaration = false,
-                        Indent = true
-                    };
-                    using (var xw = XmlWriter.Create(ms, xmlWriterSettings))
-                    {
-                        var xmlSerializer = new XmlSerializer(typeof(ClientIdentification));
-                        xmlSerializer.Serialize(xw, clientIdentification);
-                    }
+			var skoleOrgNr = xmlDoc.GetElementsByTagName("pa:Orgnr")[0].InnerText;
+			Console.WriteLine("Melder på kandidatene i {0} til skole with orgNr: {1}", xmlFile, skoleOrgNr);
 
-                    theSerializedString = Encoding.UTF8.GetString(ms.ToArray());
+			var cookie = Login(baseAddress, skoleOrgNr);
+			var cookieContainer = new CookieContainer();
 
-                    //remove BOM
-                    var byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
-                    if (theSerializedString.StartsWith(byteOrderMarkUtf8))
-                    {
-                        theSerializedString = theSerializedString.Remove(0, byteOrderMarkUtf8.Length);
-                    }
-                }
+			if (cookie != null)
+				cookieContainer.Add(cookie);
 
-                
-                xmlSignature.LoadXml(theSerializedString);
-            }
-            
-            xmlSignature.Sign();
+			var handler = new WebRequestHandler
+			{
+				CookieContainer = cookieContainer,
+				UseCookies = true,
+				UseDefaultCredentials = true
+			};
 
-            var signature = xmlSignature.ConvertToString();
-            //var validpayload = Convert.ToBase64String(Encoding.UTF8.GetBytes(signature));
-            var handler = new WebRequestHandler
-            {
-                CookieContainer = new CookieContainer(),
-                UseCookies = true,
-                UseDefaultCredentials = true
-            };
+			using (var client = new HttpClient(handler))
+			{
+				client.BaseAddress = new Uri(baseAddress);
+				var response = client.PostAsXmlAsync("/api/ekstern/kandidatpamelding", xmlDoc.DocumentElement).Result;
+				Console.WriteLine("StatusCode: {0}", response.StatusCode);
+			}
 
-            using (var client = new HttpClient(handler))
-            {
-                client.BaseAddress = new Uri(baseAddress);
-                var response = client.PostAsync("/api/ekstern/innlogging", new StringContent(signature)).Result;
-                if (!response.IsSuccessStatusCode)
-                {
-                    Console.Error.WriteLine("Response: " + response.StatusCode);
-                    Console.Error.WriteLine("So quitting...");
-                    Environment.Exit(1);
-                }
+		}
 
-                var allcookies = handler.CookieContainer.GetCookies(new Uri(baseAddress));
+		private static void IssueRequestWithCookie(Cookie cookie, string baseAddress, string relativeAddress)
+		{
+			Console.WriteLine("Press enter to try to issue request with the newly obtained cookie");
+			Console.ReadLine();
 
-                return allcookies["FedAuth"];
-            }
-        }
-    }
+			var cookieContainer = new CookieContainer();
+
+			if (cookie != null)
+				cookieContainer.Add(cookie);
+
+			var handler = new WebRequestHandler
+			{
+				CookieContainer = cookieContainer,
+				UseCookies = true,
+				UseDefaultCredentials = true
+			};
+
+			using (var client = new HttpClient(handler))
+			{
+				client.BaseAddress = new Uri(baseAddress);
+				var response = client.GetAsync(relativeAddress).Result;
+
+				Console.WriteLine(response);
+				Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+			}
+		}
+
+		private static Cookie Login(string baseAddress, string skoleOrgNr = "974589664")
+		{
+			var xmlSignature = new XmlDocument { PreserveWhitespace = true };
+
+			using (var rng = new RNGCryptoServiceProvider())
+			{
+				var nonceBytes = new byte[8];
+				rng.GetBytes(nonceBytes);
+
+				var nonce = Convert.ToBase64String(nonceBytes);
+				var timeStamp = DateTime.Now;
+				var clientIdentification = new ClientIdentification
+				{
+					Skoleorgno = skoleOrgNr,
+					Skolenavn = "Eksempel skole",
+					Brukernavn = "skoleadmin",
+					Nonce = nonce,
+					TimeStamp = timeStamp
+				};
+
+				string theSerializedString;
+				using (var ms = new MemoryStream())
+				{
+					var xmlWriterSettings = new XmlWriterSettings()
+					{
+						CloseOutput = false,
+						Encoding = Encoding.UTF8,
+						OmitXmlDeclaration = false,
+						Indent = true
+					};
+					using (var xw = XmlWriter.Create(ms, xmlWriterSettings))
+					{
+						var xmlSerializer = new XmlSerializer(typeof(ClientIdentification));
+						xmlSerializer.Serialize(xw, clientIdentification);
+					}
+
+					theSerializedString = Encoding.UTF8.GetString(ms.ToArray());
+
+					//remove BOM
+					var byteOrderMarkUtf8 = Encoding.UTF8.GetString(Encoding.UTF8.GetPreamble());
+					if (theSerializedString.StartsWith(byteOrderMarkUtf8))
+					{
+						theSerializedString = theSerializedString.Remove(0, byteOrderMarkUtf8.Length);
+					}
+				}
+
+
+				xmlSignature.LoadXml(theSerializedString);
+			}
+
+			xmlSignature.Sign();
+
+			var signature = xmlSignature.ConvertToString();
+			//var validpayload = Convert.ToBase64String(Encoding.UTF8.GetBytes(signature));
+			var handler = new WebRequestHandler
+			{
+				CookieContainer = new CookieContainer(),
+				UseCookies = true,
+				UseDefaultCredentials = true
+			};
+
+			using (var client = new HttpClient(handler))
+			{
+				client.BaseAddress = new Uri(baseAddress);
+				var response = client.PostAsync("/api/ekstern/innlogging", new StringContent(signature)).Result;
+				if (!response.IsSuccessStatusCode)
+				{
+					Console.Error.WriteLine("Response: " + response.StatusCode);
+					Console.Error.WriteLine("So quitting...");
+					Environment.Exit(1);
+				}
+
+				var allcookies = handler.CookieContainer.GetCookies(new Uri(baseAddress));
+
+				return allcookies["FedAuth"];
+			}
+		}
+	}
 }
